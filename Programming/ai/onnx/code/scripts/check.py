cd96e941cfbdf6ed146be6537e02c12e136bf4f5
robo2020 <robo2020@gmail.com>
2025-10-19 00:37:51 +0300

    Deployed d1a2162 with MkDocs version: 1.6.1

--- Programming/ai/onnx/code/scripts/check.py
+++ Programming/ai/onnx/code/scripts/check.py
@@ -1,5 +1,8 @@
 import onnxruntime as ort
 
+ort.set_default_logger_severity(0)  # 0 = VERBOSE, 1 = INFO, 2 = WARNING, 3 = ERROR, 4 = FATAL
+
+
 # Show all available providers on your machine
 print("Available providers:", ort.get_available_providers())
 print("ONNX Runtime version:", ort.__version__)
