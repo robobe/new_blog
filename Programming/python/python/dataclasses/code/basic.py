acf6edc036df7148456846518300dc845713f4fb
robobe2020 <robobe2020@gmail.com>
2025-12-29 09:07:23 +0200

    Deployed 696fad6 with MkDocs version: 1.6.1

--- Programming/python/python/dataclasses/code/basic.py
+++ Programming/python/python/dataclasses/code/basic.py
@@ -1,10 +1,29 @@
-from dataclasses import dataclass,asdict, astuple
+from dataclasses import dataclass
+from typing import Annotated
+from typing import get_type_hints, get_origin, get_args
+import struct
 
 @dataclass
-class Point:
-    x: float
-    y: float
+class Packet:
+    a: Annotated[int, "h", "xx"]   # int16
+    b: Annotated[int, "I"]   # uint32
+    c: Annotated[float, "f"] # float32
+    name: Annotated[str, "16s"]
 
-p = Point(1.0, 2.0)
-print(asdict(p))
-print(astuple(p))
+def struct_format(cls, endian="<"):
+    hints = get_type_hints(cls, include_extras=True)
+    fmt = endian
+
+    for name in cls.__dataclass_fields__:
+        t = hints[name]
+        if get_origin(t) is Annotated:
+            print(get_args(t))
+            _, fmt_char, *_ = get_args(t)
+            fmt += fmt_char
+        else:
+            raise TypeError(f"Field {name} missing struct annotation")
+
+    return fmt
+
+fmt = struct_format(Packet)
+print(fmt)  # "<hIf16s"
