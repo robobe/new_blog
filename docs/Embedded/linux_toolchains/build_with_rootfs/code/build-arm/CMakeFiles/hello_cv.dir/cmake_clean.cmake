8f170b13ce346c6bf49df9e1835ee09f614cf82d
robobe2020 <robobe2020@gmail.com>
2026-08-15 23:10:12 +0300

    150826

---
+++ docs/Embedded/linux_toolchains/build_with_rootfs/code/build-arm/CMakeFiles/hello_cv.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_cv.dir/main.cpp.o"
+  "CMakeFiles/hello_cv.dir/main.cpp.o.d"
+  "hello_cv"
+  "hello_cv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_cv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
